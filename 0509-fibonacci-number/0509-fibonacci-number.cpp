0da042dc040b04c6f7c35fa0a66e4c46c121e222
716-Anuj <93367522+716-Anuj@users.noreply.github.com>
2024-09-26 19:16:03 +0530

    Time: 2 ms (80.98%), Space: 7.6 MB (12.90%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
 
-    int f(int n, vector<int>&dp) {
-        if(n == 0) {
-            return 0;
-        }
-        if(n == 1) {
-            return 1;
-        }
+    int f(int n, vector<int> &dp) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
 
-        if(dp[n] != -1) {
-            return dp[n];
-        }
+        if(dp[n] != -1) return dp[n];
 
-        return dp[n] =  f(n-1, dp) + f(n-2, dp);
+        return dp[n] = f(n-1, dp)+f(n-2, dp);
     }
 
     int fib(int n) {
