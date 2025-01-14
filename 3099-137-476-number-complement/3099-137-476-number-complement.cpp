df36c2e5b68943b0372108be502fb0d455def29c
716-Anuj <93367522+716-Anuj@users.noreply.github.com>
2025-01-14 16:48:14 +0530

    Time: 0 ms, Memory: 8.1 MB - LeetHub

--- 3099-137-476-number-complement/3099-137-476-number-complement.cpp
+++ 3099-137-476-number-complement/3099-137-476-number-complement.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findComplement(int num) {
         int bits = floor(log2(num)) + 1;
-        unsigned int mask = (1ULL << bits) - 1;
+        int mask = (1ULL << bits) - 1;
         return num ^ mask;
     }
 };
